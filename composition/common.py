e589263e0b82058fd442feecc9d39e91ad8a75d0
kureta <skureta@gmail.com>
2021-04-11 14:48:55 +0300

    Better autotune, fixed adsr

--- composition/common.py
+++ composition/common.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 import numpy as np
 
 sample_rate = 16000
@@ -25,7 +27,7 @@ def perc(attack_ratio=0.1, duration=0.5, peak=1.0):
 
 
 def adsr(a, d, s, r, dur, peak, vol):
-    factor = dur / sum([a, d, s, r])
+    factor = sum([a, d, s, r])
     num_steps = int(dur / step_dur)
 
     attack_steps = int(num_steps * a / factor)
@@ -47,3 +49,30 @@ def silence(duration=0.5):
 
 def rescale(xs, min_val, max_val):
     return xs * (max_val - min_val) + min_val
+
+
+scales = {
+    'major': [0, 2, 4, 5, 7, 9, 11],
+    'melodic_minor': [0, 2, 3, 5, 7, 9, 11],
+    'harmonic_major': [0, 2, 4, 5, 7, 8, 11],
+    'harmonic_minor': [0, 2, 3, 5, 7, 8, 11],
+    'diminished': [0, 2, 3, 5, 6, 8, 9, 11],
+    'wholetone': [0, 2, 4, 6, 8, 10],
+    'augmented': [0, 3, 4, 7, 8, 11],
+    'chromatic': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+}
+
+
+def autotune(f0_midi, amount=0.0, scale='chromatic', rotate=0, offset=0):
+    """Reduce variance of f0 from the chromatic or scale intervals."""
+    scale = deque(scales[scale])
+    scale.rotate(rotate)
+    scale = np.array([(s - scale[0]) % 12 for s in scale])
+    scale = np.concatenate([scale + 12 * i for i in range(10)])
+    scale = (scale + offset) % 120
+
+    idx = np.abs(f0_midi[np.newaxis, :] - scale[:, np.newaxis]).argmin(axis=0)
+    midi_diff = f0_midi - scale[idx]
+
+    # Adjust the midi signal.
+    return f0_midi - amount * midi_diff
