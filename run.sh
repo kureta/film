09c4773df24e60fdd5df95520607d512d86681bc
kureta <skureta@gmail.com>
2021-04-18 19:47:56 +0300

    docker mocker

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
-#docker run --gpus all --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 --device /dev/nvidia0 -p 8888:8888 -v  "`pwd`:/src" zak-ddsp
-docker run --gpus all --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 --device /dev/nvidia0 --device /dev/nvidia-uvm --device /dev/nvidia-uvm-tools --device /dev/nvidiactl -p 8888:8888 -v  "`pwd`:/src" zak-ddsp
+docker run \
+    --gpus all \
+    --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 \
+    --device /dev/nvidia0 --device /dev/nvidia-uvm --device /dev/nvidia-uvm-tools --device /dev/nvidiactl \
+    -p 8888:8888 \
+    -v  "$(pwd):/src" \
+    zak-ddsp
